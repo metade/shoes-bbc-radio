323688dcf9a167966230e5efdd21c033fe2c2a1f
Patrick Sinclair <metade@livro.lan>
2008-05-19 08:41:34 +0100

    first commit

---
+++ shoes_bbc_radio.rb
@@ -0,0 +1,38 @@
+class ShoesRadio < Shoes
+  url '/', :index
+  url '/listen/(\w+)', :listen
+  
+  SERVICES = {
+    'radio1' => 'http://www.bbc.co.uk/radio1/wm_asx/aod/radio1.asx',
+    '6music' => 'http://www.bbc.co.uk/6music/ram/6music.asx',
+  }
+  
+  def index
+    list_services
+  end
+    
+  def listen(service)
+    stack :margin => 4, :height => 10 do
+      @vid = video(SERVICES[service])
+    end
+    para "#{service}: ",
+      link("play") { @vid.play; @label.replace('about to play...') }, ", ",
+      link("stop") { @vid.stop; @label.replace('about to stop...') }
+    @label = para @vid.playing?    
+    animate(1) { @label.replace @vid.playing? ? 'playing' : 'stopped' }
+    
+    stack :margin => 4 do
+      
+    end
+  end
+
+  private
+  
+  def list_services(current=nil)
+    SERVICES.keys.each do |s| 
+      para ((s==current) ? s : link(s, :click => "/listen/#{s}"))
+    end    
+  end
+end
+
+Shoes.app :title => "Shoes BBC Radio", :width => 300, :height => 500, :radius => 12, :resizable => true
